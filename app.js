88262e22503fec15a233c56872da8bec81c855fa
clark <clark_mitchelljr@hotmail.com>
2022-12-08 09:04:25 -0700

    added files/folders

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use("/static", express.static("public"));
+
+
+app.set('view engine', 'pug')
+
+app.use((req, res, next) => {
+  const err = new Error("Not Found");
+  err.status = 404;
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render("error", err);
+});
+
+app.listen(port, () => {
+  console.log(`The application is running on port: ${port}`);
+});
