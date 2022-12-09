b6aa8a9f26303b37fbd0813c21c3f02c999a65b1
clark <clark_mitchelljr@hotmail.com>
2022-12-08 18:03:48 -0700

    updated

--- app.js
+++ app.js
@@ -7,37 +7,41 @@ const { projects } = require("./data/projectData.json");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/static", express.static("public"));
 
-
+//set view to pug
 app.set('view engine', 'pug')
 
+
+//set route for home page
 app.get("/", (req, res) => {
   res.render("index", { projects });
 });
 
+//set route for about page
 app.get("/about", (req, res) => {
   res.render("about");
 });
 
-
+//set route for projects based on id
 app.get("/projects/:id", (req, res) => {
   const { id } = req.params;
   const project = projects[id];
   res.render("project", { project });
 });
 
-
+//404 error handler
 app.use((req, res, next) => {
   const err = new Error("Not Found");
   err.status = 404;
   next(err);
 });
 
+//General error handler
 app.use((err, req, res, next) => {
     if (err) {
       console.log("Global error handler called", err);
     }
     if (err.status === 404) {
-      res.status(404).render("not-found", { err });
+      res.status(404).render("page-not-found", { err });
     } else {
       err.status = res.statusCode === 200 ? 500 : res.statusCode;
       res.status(err.status).render("error", { err });
